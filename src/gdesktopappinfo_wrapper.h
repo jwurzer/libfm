ae28fe71fa8cabf721e17cdc14c6d01360d362e4
jwurzer <jwurzer.development@gmail.com>
2024-10-01 15:42:25 +0200

    Using HAVE_X11 macro

--- src/gdesktopappinfo_wrapper.h
+++ src/gdesktopappinfo_wrapper.h
@@ -1,13 +1,16 @@
 #ifndef GDESKTOPAPPINFO_WRAPPER_H
 #define GDESKTOPAPPINFO_WRAPPER_H
 
-#if 0
+#ifdef HAVE_X11
+
 #include <gio/gdesktopappinfo.h>
-#else
 
-//#include "gdesktopappinfo_per_gosxappinfo.h"
-#include "gdesktopappinfo_per_gappinfo.h"
+#else // HAVE_X11
 
-#endif
+#include "gdesktopappinfo_per_gosxappinfo.h"
+//#include "gdesktopappinfo_per_gappinfo.h"
+
+#endif // HAVE_X11
+
 #endif
 
